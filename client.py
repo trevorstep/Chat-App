ffa3985e21792a17f218d461605f56a6883dbf9f
Trevor Stephenson <trevors19themail@gmail.com>
2025-03-30 21:40:22 -0600

    Made server and client

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket  
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+client_socket.connect(("localhost", 12345))  
+
+while True:  
+    message = input("Enter message: ")  
+    if message.lower() == "exit":  
+        break  
+    client_socket.sendall(message.encode())   
+    data = client_socket.recv(1024) 
+    print(f"Server: {data.decode()}")  
+
+client_socket.close()  
